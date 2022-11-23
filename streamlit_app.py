5c974b30ed546ff18dd2a42724e44bd32ac2b67f
Andrea Bragantini <andrea.aldo.bragantini@gmail.com>
2022-11-23 12:54:46 +0100

    Update streamlit_app.py
    small changes and documented code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from collections import namedtuple
-import altair as alt
 import math
 import pandas as pd
 import numpy as np
-import plost
-from PIL import Image
+import plost                # this package is used to create plots/charts within streamlit
+from PIL import Image       # this package is used to put images within streamlit
+
+from api_connection import get_data_from_api
 
 # Page setting
 st.set_page_config(layout="wide")
@@ -16,7 +17,9 @@ with open('style.css') as f:
 # Data
 seattle_weather = pd.read_csv('https://raw.githubusercontent.com/tvst/plost/master/data/seattle-weather.csv', parse_dates=['date'])
 stocks = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/stocks_toy.csv')
+# replace the previous data with your own streamed data from API
 
+### Here starts the web app design
 # Row A
 a1, a2, a3 = st.columns(3)
 a1.image(Image.open('streamlit-logo-secondary-colormark-darktext.png'))
@@ -33,8 +36,8 @@ b4.metric("Humidity", "86%", "4%")
 # Row C
 c1, c2 = st.columns((7,3))
 with c1:
-    st.markdown('### Heatmap')
-    plost.time_hist(
+    st.markdown('### Heatmap')              # text is created with markdown
+    plost.time_hist(                        # histogram
     data=seattle_weather,
     date='date',
     x_unit='week',
@@ -44,7 +47,7 @@ with c1:
     legend=None)
 with c2:
     st.markdown('### Bar chart')
-    plost.donut_chart(
+    plost.donut_chart(                      # donut charts
         data=stocks,
         theta='q2',
         color='company')
